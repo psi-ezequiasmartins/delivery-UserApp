b9d31ee7f8082de2acbc1453485d1d8d62b47bd2
Ezequias Martins <13648451+by-psi@users.noreply.github.com>
2025-03-19 09:18:37 -0300

    19032025-0917
    Neste código, o Stack.Navigator define a rota inicial como Home. O TabNavigator inclui todas as rotas necessárias para a navegação por abas. O DrawerNavigator gerencia a navegação lateral. O OrderStackNavigator é usado para gerenciar a navegação após a confirmação do pedido, garantindo que todas as rotas estejam disponíveis no navigation.navigate. (aided by CoPilot)

--- src/routes/App.Routes.js
+++ src/routes/App.Routes.js
@@ -1,7 +1,3 @@
-/**
-* src/routes/App.Routes.js
-*/
-
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -22,119 +18,141 @@ const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
 
-export default function AppRoutes() {
-
-  function TabNavigator() {
-    return (
-      <Tab.Navigator
-        screenOptions={{
-          tabBarStyle: { height: 65 },
-          fontWeight: 'bold',
-          headerShown: false
-        }}
-      >
-        <Tab.Screen
-          name='Home'
-          component={ Home }
-          options={{
-            tabBarIcon: ({ color, size, focused }) => {
-              return <Entypo name='shop' color={(focused !== true) ? '#5D5D5D' : '#000'} size={35} />
-            },
-          }}
-        />
-        <Tab.Screen
-          name='Meus Pedidos'
-          component={ OrderStackNavigator }
-          options={{
-            tabBarIcon: ({ color, size, focused }) => {
-              return <Fontisto name='shopping-bag-1' color={(focused !== true) ? '#5D5D5D' : '#000'} size={35} />
-            },
-          }}
-        />
-        <Tab.Screen
-          name='Perfil do Usuário'
-          component={ Perfil }
-          options={{
-            tabBarIcon: ({ color, size, focused }) => {
-              return <FontAwesome5 name='user-cog' color={(focused !== true) ? '#5D5D5D' : '#000'} size={35} />
-            },
-          }}
-        />
-      </Tab.Navigator>
-    );
-  }
-
-  function DrawerNavigator() {
-
-    function getHeaderTitle(route) {
-      const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
-      switch ( routeName ) {
-        case 'Home': return 'psi-Delivery';
-        case 'Pedidos': return 'Meus Pedidos';
-        case 'Perfil': return 'Dados do Usuário';
-      }
-    };
-
-    return (
-      <Drawer.Navigator
-        drawerContent={(props) => <SideBar {...props} />}
-        screenOptions={{
-          headerShown: true,
-          drawerStyle: {
-            backgroundColor: '#FFF',
-            width: '70%',
-            marginTop: 5,
-            marginBotton: 5,
-            borderTopRightRadius: 25,
-            borderBottomRightRadius: 25,
-          },
-          drawerLabelStyle: {
-            fontWeight: 'bold'
-          },
-          drawerItemStyle: {
-            activeTintColor: '#FFF',
-            activeBackgroundColor: '#FF0000',
-            inactiveTintColor: '#5D5D5D',
-            inactiveBackgroundColor: '#000',
-            marginVertical: 5
-          },
-        }}
-      >
-        <Drawer.Screen
-          name="psi-Delivery"
-          component={ TabNavigator }
-          options={({ route }) => ({
-            headerTitle: getHeaderTitle(route),
-            headerTintColor: '#FFF',
-            headerStyle: {
-              backgroundColor: '#000',
-              borderBottomWidth: 0,
-            },
-            tabBarIcon: {
-              color: '#000'
-            }
-          })}
-        />
-      </Drawer.Navigator>
-    );
-  }
-
+function TabNavigator() {
   return (
-    <Stack.Navigator 
-      screenOptions={{ 
-        headerShown: false 
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: { height: 65 },
+        fontWeight: 'bold',
+        headerShown: false
       }}
     >
-      <Stack.Screen 
+      <Tab.Screen
+        name='Home'
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => {
+            return <Entypo name='shop' color={(focused !== true) ? '#5D5D5D' : '#000'} size={35} />
+          },
+        }}
+      />
+      <Tab.Screen
+        name='Meus Pedidos'
+        component={OrderStackNavigator}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => {
+            return <Fontisto name='shopping-bag-1' color={(focused !== true) ? '#5D5D5D' : '#000'} size={35} />
+          },
+        }}
+      />
+      <Tab.Screen
+        name='Perfil do Usuário'
+        component={Perfil}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => {
+            return <FontAwesome5 name='user-cog' color={(focused !== true) ? '#5D5D5D' : '#000'} size={35} />
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function DrawerNavigator() {
+
+  function getHeaderTitle(route) {
+    const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
+    switch (routeName) {
+      case 'Home': return 'psi-Delivery';
+      case 'Pedidos': return 'Meus Pedidos';
+      case 'Perfil': return 'Dados do Usuário';
+    }
+  };
+
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <SideBar {...props} />}
+      screenOptions={{
+        headerShown: true,
+        drawerStyle: {
+          backgroundColor: '#FFF',
+          width: '70%',
+          marginTop: 5,
+          marginBotton: 5,
+          borderTopRightRadius: 25,
+          borderBottomRightRadius: 25,
+        },
+        drawerLabelStyle: {
+          fontWeight: 'bold'
+        },
+        drawerItemStyle: {
+          activeTintColor: '#FFF',
+          activeBackgroundColor: '#FF0000',
+          inactiveTintColor: '#5D5D5D',
+          inactiveBackgroundColor: '#000',
+          marginVertical: 5
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="psi-Delivery"
+        component={TabNavigator}
+        options={({ route }) => ({
+          headerTitle: getHeaderTitle(route),
+          headerTintColor: '#FFF',
+          headerStyle: {
+            backgroundColor: '#000',
+            borderBottomWidth: 0,
+          },
+          tabBarIcon: {
+            color: '#000'
+          }
+        })}
+      />
+    </Drawer.Navigator>
+  );
+}
+
+const OrdersStack = createStackNavigator();
+
+function OrderStackNavigator() {
+  return (
+    <OrdersStack.Navigator screenOptions={{ headerShown: false }}>
+      <OrdersStack.Screen
+        name="Orders"
+        component={Pedidos}
+        options={{ headerShown: false }}
+      />
+      <OrdersStack.Screen
+        name="OrderDetails"
+        component={OrderDetailsNavigator}
+        options={{ headerShown: false }}
+      />
+    </OrdersStack.Navigator>
+  );
+}
+
+export default function AppRoutes() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+      />
+      <Stack.Screen
         name="DrawerNavigator"
-        component={ DrawerNavigator }
+        component={DrawerNavigator}
       />
-      <Stack.Screen 
-        name="Home" 
-        component={ Home } 
-        options={{ 
-          headerShown: true, 
-          headerTitle: 'Lista de Categorias', 
+      <Stack.Screen
+        name="Deliveries"
+        component={Deliveries}
+        options={{
+          headerShown: true,
+          headerTitle: 'Lista de Deliveries',
           headerTintColor: '#FFF',
           headerStyle: {
             backgroundColor: '#000',
@@ -142,24 +160,11 @@ export default function AppRoutes() {
           },
         }}
       />
-      <Stack.Screen 
-        name="Deliveries" 
-        component={ Deliveries } 
-        options={{ 
-          headerShown: true, 
-          headerTitle: 'Lista de Deliveries', 
-          headerTintColor: '#FFF',
-          headerStyle: {
-            backgroundColor: '#000',
-            borderBottomWidth: 0,
-          },
-        }}
-      />
-      <Stack.Screen 
-        name="DeliveryInfo" 
-        component={ DeliveryInfo } 
-        options={{ 
-          headerShown: true, 
+      <Stack.Screen
+        name="DeliveryInfo"
+        component={DeliveryInfo}
+        options={{
+          headerShown: true,
           headerTitle: 'Delivery Info',
           headerTintColor: '#FFF',
           headerStyle: {
@@ -168,11 +173,11 @@ export default function AppRoutes() {
           },
         }}
       />
-      <Stack.Screen 
-        name="Cesta" 
-        component={ Cesta } 
-        options={{ 
-          headerShown: true, 
+      <Stack.Screen
+        name="Cesta"
+        component={Cesta}
+        options={{
+          headerShown: true,
           headerTitle: 'Cesta de Compras',
           headerTintColor: '#FFF',
           headerStyle: {
@@ -181,11 +186,11 @@ export default function AppRoutes() {
           },
         }}
       />
-      <Stack.Screen 
-        name="Pedidos" 
-        component={ Pedidos } 
-        options={{ 
-          headerShown: true, 
+      <Stack.Screen
+        name="Pedidos"
+        component={Pedidos}
+        options={{
+          headerShown: true,
           headerTitle: 'Meus Pedidos',
           headerTintColor: '#FFF',
           headerStyle: {
@@ -194,11 +199,11 @@ export default function AppRoutes() {
           },
         }}
       />
-      <Stack.Screen 
-        name="Perfil" 
-        component={ Perfil } 
-        options={{ 
-          headerShown: true, 
+      <Stack.Screen
+        name="Perfil"
+        component={Perfil}
+        options={{
+          headerShown: true,
           headerTitle: 'Dados do Usuário',
           headerTintColor: '#FFF',
           headerStyle: {
@@ -210,227 +215,3 @@ export default function AppRoutes() {
     </Stack.Navigator>
   );
 }
-
-const OrdersStack = createStackNavigator();
-
-function OrderStackNavigator() {
-  return (
-    <OrdersStack.Navigator screenOptions={{ headerShown: false }}>
-      <OrdersStack.Screen 
-        name="Orders"
-        component={ Pedidos } 
-        screenOptions={{ headerShown: false }}
-      />
-      <OrdersStack.Screen
-        name="OrderDetails"
-        component={ OrderDetailsNavigator }
-        screenOptions={{ headerShown: false }}
-      />
-    </OrdersStack.Navigator>
-  );
-};
-
-/**
- * 
- 
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
-import { FontAwesome5, Entypo, Fontisto } from '@expo/vector-icons';
-
-import Grupos from '../pages/Grupos';
-import Deliveries from "../pages/Delivery";
-import DeliveryInfo from "../pages/Delivery/DeliveryInfo";
-import Cesta from '../pages/Cesta';
-import Pedidos from "../pages/Pedidos";
-import Perfil from "../pages/User";
-
-import SideBar from '../components/SideBar';
-import OrderDetailsNavigator from '../pages/Pedidos/OrderDetailsNavigator';
-
-const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
-const Tab = createBottomTabNavigator();
-
-export default function AppRoutes() {
-
-  function TabNavigator() {
-    return (
-      <Tab.Navigator
-        screenOptions={{
-          tabBarStyle: { height: 65 },
-          fontWeight: 'bold',
-          headerShown: false,
-        }}
-      >
-        <Tab.Screen
-          name='Grupos'
-          component={ Grupos }
-          options={{
-            tabBarIcon: ({ color, size, focused }) => {
-              return <Entypo name='shop' color={(focused !== true) ? '#5D5D5D' : '#000'} size={35} />
-            },
-          }}
-        />
-        <Tab.Screen
-          name='Pedidos'
-          component={ OrderStackNavigator }
-          options={{
-            tabBarIcon: ({ color, size, focused }) => {
-              return <Fontisto name='shopping-bag-1' color={(focused !== true) ? '#5D5D5D' : '#000'} size={35} />
-            },
-          }}
-        />
-        <Tab.Screen
-          name='Perfil'
-          component={ Perfil }
-          options={{
-            tabBarIcon: ({ color, size, focused }) => {              
-              return <FontAwesome5 name='user-cog' color={(focused !== true) ? '#5D5D5D' : '#000'} size={35} />
-            },
-          }}
-        />
-      </Tab.Navigator>
-    );
-  }
-
-  function DrawerNavigator() {
-
-    const getHeaderTitle = (route) => {
-      const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
-      switch ( routeName ) {
-        case 'Grupos': return 'Grupos de Delivery';
-        case 'Pedidos': return 'Meus Pedidos';
-        case 'Perfil': return 'Dados do Usuário';
-      }
-    };
-
-    return (
-      <Drawer.Navigator
-        drawerContent={(props) => <SideBar {...props} />}
-        screenOptions={{
-          headerShown: true,
-          drawerStyle: {
-            backgroundColor: '#FFF',
-            width: '75%',
-            marginTop: 90,
-            borderTopRightRadius: 15,
-            borderBottomRightRadius: 15,
-          },
-          drawerLabelStyle: {
-            fontWeight: 'bold'
-          },
-          drawerItemStyle: {
-            activeTintColor: '#FFF',
-            activeBackgroundColor: '#FF0000',
-            inactiveTintColor: '#5D5D5D',
-            inactiveBackgroundColor: '#000',
-            marginVertical: 5
-          },
-        }}
-      >
-        <Drawer.Screen
-          name="psi-Delivery"
-          component={ TabNavigator }
-          options={({ route }) => ({
-            headerTitle: getHeaderTitle(route),
-            headerTintColor: '#FFF',
-            headerStyle: {
-              backgroundColor: '#000',
-              borderBottomWidth: 0,
-            },
-            tabBarIcon: {
-              color: '#000'
-            }
-          })}
-        />
-      </Drawer.Navigator>
-    );
-  }
-
-  return (
-    <Stack.Navigator 
-      screenOptions={{ 
-        headerShown: false 
-      }}
-    >
-      <Stack.Screen 
-        name="DrawerNavigator"
-        component={ DrawerNavigator }
-      />
-      <Stack.Screen 
-        name="Deliveries" 
-        component={ Deliveries } 
-        options={{ 
-          headerShown: true, 
-          headerTitle: 'Deliveries', 
-          headerTintColor: '#FFF',
-          headerStyle: {
-            backgroundColor: '#000',
-            borderBottomWidth: 0,
-          },
-        }}
-      />
-      <Stack.Screen 
-        name="DeliveryInfo" 
-        component={ DeliveryInfo } 
-        options={{ 
-          headerShown: true, 
-          headerTitle: 'Delivery Info',
-          headerTintColor: '#FFF',
-          headerStyle: {
-            backgroundColor: '#000',
-            borderBottomWidth: 0,
-          },
-        }}
-      />
-      <Stack.Screen 
-        name="Cesta" 
-        component={ Cesta } 
-        options={{ 
-          headerShown: true, 
-          headerTitle: 'Cesta de Compras',
-          headerTintColor: '#FFF',
-          headerStyle: {
-            backgroundColor: '#000',
-            borderBottomWidth: 0,
-          },
-        }}
-      />
-      <Stack.Screen 
-        name="Pedidos" 
-        component={ Pedidos } 
-        options={{ 
-          headerShown: true, 
-          headerTitle: 'Meus Pedidos',
-          headerTintColor: '#FFF',
-          headerStyle: {
-            backgroundColor: '#000',
-            borderBottomWidth: 0,
-          },
-        }}
-      />
-    </Stack.Navigator>
-  );
-}
-
-const OrdersStack = createStackNavigator();
-
-function OrderStackNavigator() {
-  return (
-    <OrdersStack.Navigator screenOptions={{ headerShown: false }}>
-      <OrdersStack.Screen 
-        name="Orders" 
-        component={ Pedidos } 
-        screenOptions={{ headerShown: false }}
-      />
-      <OrdersStack.Screen
-        name="OrderDetails"
-        component={ OrderDetailsNavigator }
-        screenOptions={{ headerShown: false }}
-      />
-    </OrdersStack.Navigator>
-  );
-};
-*/
