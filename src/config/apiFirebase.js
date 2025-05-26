4147526ac516bbd6e29e99fef04df1d9268ab3f0
Ezequias Martins <ezequiasmartins@gmail.com>
2025-05-25 23:45:15 -0300

    25052025-234510

--- src/config/apiFirebase.js
+++ src/config/apiFirebase.js
@@ -3,14 +3,14 @@
  */
 
 import { initializeApp } from 'firebase/app';
-import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-
-import { API_KEY, AUTH_DOMAIN, DATABASE_URL, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env';
+import { 
+  API_KEY, AUTH_DOMAIN, DATABASE_URL, PROJECT_ID, 
+  STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID
+} from '@env';
 
 const firebaseConfig = {
   apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
+  authDomain: AUTH_DOMAIN, 
   databaseURL: DATABASE_URL,
   projectId: PROJECT_ID,
   storageBucket: STORAGE_BUCKET,
@@ -18,14 +18,4 @@ const firebaseConfig = {
   appId: APP_ID
 };
 
-const firebase_app = initializeApp(firebaseConfig);  // Inicialize o Auth corretamente para React Native
-let auth;
-try {
-  auth = initializeAuth(firebase_app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-  });
-} catch (e) {
-  auth = getAuth(firebase_app); // Se já foi inicializado, use getAuth
-}
-
-export { firebase_app, auth };
+const firebase_app = initializeApp(firebaseConfig);
