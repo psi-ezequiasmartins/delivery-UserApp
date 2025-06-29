169b96933aa7d38d630a5df969f2e977968552db
Ezequias Martins <ezequiasmartins@gmail.com>
2025-06-29 08:20:24 -0300

    29062025-082012

--- src/config/apiAxios.js
+++ src/config/apiAxios.js
@@ -12,7 +12,7 @@ if (isDevelopment) {
   console.log('Ambiente de desenvolvimento detectado. Habilitando logs detalhados.');
 }
 
-API_URL = BASE_URL || 'http://localhost:3357' || 'https://srv.deliverybairro.com';
+API_URL = BASE_URL || 'https://srv.deliverybairro.com'|| 'http://localhost:3357' ;
 
 if (isDevelopment) {
   console.log('URL da API:', API_URL);
